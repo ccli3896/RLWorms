fixed buffer transfer and env normalization

09_28_SAC/fake_worm_cont.py
@@ -55,7 +55,7 @@ class FakeWorm(gym.Env):
         obs = np.array([random.choice(state_vec),random.choice(state_vec)])
         self.state = obs
 
-        if self.steps > 1000:
+        if self.steps > 100:
             finished = True
         else:
             finished = False

10_08_rlkit/worm_env_cont.py
@@ -5,6 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+##########################
+# This is a version where observations are normalized to [-1,1] WITHIN this environment.
+# Did it because the normalized box wrapper seemed buggy and wasn't normalizing
+# the first observation for each epoch.
+##########################
+
+
 def add_to_traj(trajectory,info):
     # appends each key in info to the corresponding key in trajectory.
     # If trajectory is empty, returns trajectory as copy of info but with each element as list
@@ -34,7 +41,7 @@ class ProcessedWorm(gym.Env):
         # They must be gym.spaces objects
 
         self.action_space = spaces.Box(low=0,high=1,shape=(1,),dtype=np.float32)
-        self.observation_space = spaces.Box(low=np.array([-180,-180]), high=np.array([180,180]), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=np.array([-1,-1]), high=np.array([1,1]), dtype=np.uint8)
 
         self.target = target
         self.ep_len = ep_len
@@ -89,7 +96,7 @@ class ProcessedWorm(gym.Env):
         self.worm = worms[0]
         body_dir = relative_angle(self.worm['body'], self.target)
         head_body = relative_angle(self.worm['angs'][0], self.worm['body'])
-        obs = np.array([body_dir, head_body])
+        obs = np.array([body_dir, head_body])/180.
         
         # Find reward and then update last_loc variable
         reward = proj(self.worm['loc']-self.last_loc, [np.cos(self.target*pi/180),-np.sin(self.target*pi/180)])