bbef2f5f4a1ba4e6dcfe9424e6523884540d7172
Chenguang Li <ccli.3896@gmail.com>
2020-11-27 17:55:24 -0500

    agentdev

---
+++ 10_30_model/11_27_plain_mod.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+import sys
+
+import bin_utils as ut
+import bin_model_env as me
+
+fnames=[
+    # First worm
+    'Data/traj12-11-2020_19-04-41.pkl', #none
+    'Data/traj12-11-2020_19-14-38.pkl', #none
+    'Data/traj12-11-2020_19-24-30.pkl', #xlim 800
+    # 'Data/traj12-11-2020_19-35-31.pkl', #none # Seems like an especially bad dataset. Actually ruined all the others
+    # Second worm
+    'Data/traj12-11-2020_19-55-19.pkl', #none
+    'Data/traj12-11-2020_20-05-11.pkl', #none
+    'Data/traj12-11-2020_20-15-17.pkl', #none
+    'Data/traj12-11-2020_20-25-06.pkl', #xlim 1430
+]
+
+xlims = [1e6,1e6,800,1e6,1e6,1e6,1430]
+
+traj_df = ut.make_df(fnames,xlimit=xlims,time_steps=10)
+dist_dict = ut.make_dist_dict(traj_df)
+
+
+'''
+Setting hyperparameters
+'''
+i = int(sys.argv[1])
+alphas = [.1, .05, .01, .005, .001]
+gammas = [.7, .8, .9, .95, .98]
+epsilons = [.1, .05, .01]
+runtimes = [10000, 20000]
+
+alpha_ind = i//30
+gamma_ind = (i-(30*alpha_ind))//6
+epsilon_ind = (i-30*alpha_ind-6*gamma_ind)//2
+runtime_ind = (i-30*alpha_ind-6*gamma_ind-2*epsilon_ind)
+
+alpha = alphas[alpha_ind]
+gamma = gammas[gamma_ind]
+epsilon = epsilons[epsilon_ind]
+runtime = runtimes[runtime_ind]
+
+fbase = './Outputs/A'+str(alpha_ind)+'G'+str(gamma_ind)+'E'+str(epsilon_ind)+'R'+str(runtime_ind)
+'''
+
+'''
